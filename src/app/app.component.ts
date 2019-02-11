70e0501fbf4cc4cbee07fa8c8177ea52dcbc0e6d
DDEAL\moussa <m.sow@deal.fr>
2019-02-11 13:51:10 +0100

    todoList avec des observables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Todo } from './todo.model';
+import { TodoService } from './todo.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'testAngular';
+  public todos$: Observable<Todo[]> = this.todoService.todos$;
+  public message: string;
+
+  constructor(private todoService: TodoService) {}
+
+  public addTodo() {
+    this.todoService.addTodo({ message: this.message, done: false });
+  }
+
+  public toggleTodo(index: number) {
+    this.todoService.toggleTodo(index);
+  }
+
+  public deleteTodo(index: number) {
+    this.todoService.deleteTodo(index);
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { AppComponent } from './app.component';
+import { TodoService } from './todo.service';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports:      [ BrowserModule, FormsModule, FlexLayoutModule ],
+  declarations: [ AppComponent ],
+  providers:    [ TodoService ],
+  bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
